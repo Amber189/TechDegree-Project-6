2c1f3f960e6e90161f456c1c9ced8181f4f8f321
Amber <32441978+Amber189@users.noreply.github.com>
2018-05-22 12:44:56 -0500

    add jump video to text and pointer cursor

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-const video = document.getElementById("video");
-const text = document.querySelectorAll('span');
-
-
 //Adds video button features
 $('video').mediaelementplayer({
   features:['playpause','current','progress','volume','fullscreen']
 });
 
 
+var video = document.getElementById("video");
+var text = document.querySelectorAll('span');
+
+
 // cycle through text and hi-light in-time with video
-video.addEventListener('timeupdate', ()=> {
+video.addEventListener('timeupdate', () => {
     for (let i=0; i <text.length; i++){
-      if (video.currentTime >= text[i].dataset.start && video.currentTime <= text[i].dataset.end){
+
+      if (video.currentTime >= text[i].getAttribute("data-start") && video.currentTime <= text[i].getAttribute("data-end")){
       text[i].style.color = 'orange';
   } else {
     text[i].style.color = 'black';
@@ -21,8 +22,13 @@ video.addEventListener('timeupdate', ()=> {
 
 // if text is "clicked" jump video to that time
 
-//   for (let i = 0; i < text.length; i++){
-//     text[i].addEventListener('click', ()=> {
-//
-//   });
-// }
+  for (let i = 0; i < text.length; i++){
+    text[i].addEventListener('click', () => {
+      let jump =  text[i].getAttribute('data-start');
+         video.currentTime = jump;
+  });
+}
+
+// document.addEventListener('click', (event) => {
+//   console.log(event.target);
+// });
