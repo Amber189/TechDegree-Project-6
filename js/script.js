9c5f4e627c21943480c5c0eca0fab01fb8cddc44
Amber <32441978+Amber189@users.noreply.github.com>
2018-05-21 19:17:25 -0500

    edit to fix bug

--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
 const video = document.getElementById("video");
 const text = document.querySelectorAll('span');
 
+
+//Adds video button features
 $('video').mediaelementplayer({
-  features:['playpause','progress','volume','fullscreen']
+  features:['playpause','current','progress','volume','fullscreen']
 });
 
 
-
+// cycle through text and hi-light in-time with video
 video.addEventListener('timeupdate', ()=> {
     for (let i=0; i <text.length; i++){
       if (video.currentTime >= text[i].dataset.start && video.currentTime <= text[i].dataset.end){
-      text[i].style.backgroundColor = 'silver';
+      text[i].style.color = 'orange';
   } else {
-    text[i].style.backgroundColor = 'white';
+    text[i].style.color = 'black';
   }
  }
 });
 
+// if text is "clicked" jump video to that time
 
-// video.ontimeupdate = () => {
-//   console.log(video.currentTime);
+//   for (let i = 0; i < text.length; i++){
+//     text[i].addEventListener('click', ()=> {
+//
+//   });
 // }
